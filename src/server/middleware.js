d1930698d4700d518c244e68b5b5a85ff934ea95
Max Milton <max@wearegenki.com>
2018-09-30 23:17:41 +1000

    Improve server logger

--- src/server/middleware.js
+++ src/server/middleware.js
@@ -4,11 +4,19 @@ import * as colors from 'colorette';
  * Log server requests.
  */
 export function log(req, res, next) {
+  const start = process.hrtime();
+
+  function writeLog() {
+    const durration = process.hrtime(start);
   const { method, originalUrl, url } = req;
   const { statusCode } = res;
   /* eslint-disable-next-line no-nested-ternary */
-  const color = statusCode > 400 ? 'red' : statusCode > 300 ? 'yellow' : 'green';
-  console.log(`» ${colors[color](statusCode)} - ${method} ${originalUrl || url}`);
+    const color = statusCode >= 400 ? 'red' : statusCode >= 300 ? 'yellow' : 'green';
+    const timing = `${(durration[1] / 1e6).toFixed(2)}ms`;
+    console.log(`» ${timing} ${colors[color](statusCode)} ${method} ${originalUrl || url}`);
+  }
+  res.on('finish', writeLog);
+  res.on('error', writeLog);
   next();
 }
 
