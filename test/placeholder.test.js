7bbd002a3135c02788eeca9327109586331c5b68
Max Milton <max@wearegenki.com>
2018-10-01 08:54:13 +1000

    Add placeholder test

---
+++ test/placeholder.test.js
@@ -0,0 +1,10 @@
+/** @jest-environment node */
+
+'use strict';
+
+describe('Placeholder', () => {
+  it('will always pass', () => {
+    expect.assertions(1);
+    expect(1 + 2).toBe(3);
+  });
+});
