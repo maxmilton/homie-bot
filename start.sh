874813e817a83be3fc3dab84b445028afc1f5efa
Max Milton <max@wearegenki.com>
2018-10-14 10:59:18 +1100

    Update start/stop scripts

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 set -euo pipefail
+
 export NODE_ENV=production
 export PORT=8080
 nohup node ./dist/index.js &
 PID=$!
+
 echo $PID > /tmp/homie-bot.pid
+
 echo "Started node server with PID ${PID}"
-echo "Closing this log (ctrl + c) will not stop the server."
-tail -f ./nohup.out
+echo ""
+echo "To view logs, run this command:"
+echo "  tail -f ./nohup.out"
+echo ""
--- stop.sh
+++ stop.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -euo pipefail
+
 PID=$(cat /tmp/homie-bot.pid)
 kill $PID
