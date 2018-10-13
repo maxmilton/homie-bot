705787f272af562d1ed39806bb0a8238d08081f5
Max Milton <max@wearegenki.com>
2018-10-13 21:45:46 +1100

    Use TypeScript for tests

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,12 @@
 
 module.exports = {
   preset: '@minna-ui/jest-config',
-  collectCoverageFrom: [
-    'src/**/*.{js,html}',
-  ],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    // '^.+\\.m?js$': 'babel-jest',
+    // '^.+\\.(html|svg)$': '@minna-ui/jest-config/lib/svelte-transform.js',
+    // '^.+\\.css$': '@minna-ui/jest-config/lib/null-transform.js',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
+  collectCoverageFrom: ['src/**/*.{js,html,ts}'],
 };
