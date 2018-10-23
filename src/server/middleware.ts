72082fec8f2776d19edc2f3ba823d4089d7abae9
Max Milton <max@wearegenki.com>
2018-10-24 08:01:46 +1100

    Improve logger

--- src/server/middleware.ts
+++ src/server/middleware.ts
@@ -3,19 +3,39 @@
 import * as colors from 'colorette';
 import { IReq, IRes, Next } from './types';
 
+/** Byte size units. Let's hope our requests never get above `kB` ;) */
+const units = ['B', 'kB', 'MB', 'GB', 'TB'];
+
+/**
+ * Convert bytes into a human readable form.
+ */
+function humanizeSize(bytes: number): string {
+  const index = Math.floor(Math.log(bytes) / Math.log(1024));
+  return `${+(bytes / 1024 ** index).toFixed(2)} ${units[index]}`;
+}
+
 /**
  * Log server requests.
  */
 export function log(req: IReq, res: IRes, next: Next) {
   const start = process.hrtime();
+  const write = res.write.bind(res);
+  let byteLength = 0;
+
+  res.write = function writeFn(data) {
+    if (data) byteLength += data.length;
+    write(...arguments);
+  };
 
   function writeLog() {
-    const durration = process.hrtime(start);
+    const duration = process.hrtime(start);
     const { method, originalUrl, url } = req;
     const { statusCode } = res;
     const color = statusCode >= 400 ? 'red' : statusCode >= 300 ? 'yellow' : 'green';
-    const timing = `${(durration[1] / 1e6).toFixed(2)}ms`;
-    console.log(`» ${timing} ${colors[color](statusCode)} ${method} ${originalUrl || url}`);
+    const timing = `${+(duration[1] / 1e6).toFixed(2)}ms`;
+    const size = humanizeSize(byteLength);
+    /* tslint:disable-next-line max-line-length */
+    console.log(`» ${timing} ${colors[color](statusCode)} ${method} ${originalUrl || url} ${colors.cyan(size)}`);
   }
   res.on('finish', writeLog);
   res.on('error', writeLog);
@@ -32,11 +52,8 @@ export function parse(req: IReq, res: IRes, next: Next) {
   });
   req.on('end', () => {
     req.rawBody = data;
-    if (req.headers['content-type']
-      && req.headers['content-type'].indexOf('application/json') !== -1
-      && data
-      && (data.indexOf('{') === 0 || data.indexOf('[') === 0)
-    ) {
+    const contentType = req.headers['content-type'];
+    if (contentType && contentType.indexOf('application/json') === 0) {
       req.body = JSON.parse(data);
     }
     next();
